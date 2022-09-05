240d624517e6327dd4536050ed215cbbb4db2efb
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-09-05 15:17:49 -0300

    componente button

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
+  function alert(){
+    alert("click en boton")
+  }
   return (
     <div>
-
+      <Button type="button" nombre="boton" handleFunction={alert}/>
     </div>
   );
 }
