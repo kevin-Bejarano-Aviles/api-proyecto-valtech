1cb15c4b278c5972b17e35f7aeac1218def3a7f2
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-09-05 17:31:37 -0300

    cambios en boton y comienzo de validaciones en login

--- front/src/components/Button.jsx
+++ front/src/components/Button.jsx
@@ -4,9 +4,8 @@ function Button({type,nombre,handleFunction}) {
   //hice que la funcion no este aun para que no afecte en el renderizado
 
   return (
-    <div className="bg-green-500">
-    <button  type={type} onClick={handleFunction}>{nombre}</button>
-  </div>
+    <button className=" bg-sky-500 hover:bg-sky-700 text-white"  type={type}  onClick={handleFunction}>{nombre}</button>
+  
   )
 }
 
--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+
 function LoginPage() {
+
+  const [datos,setDatos] = useState({
+    email:'',
+    password:''
+  })
+
+  const alerta = (e)=>{
+    e.preventDefault();
+    console.log(`la contraseña es ${datos.password} y el email es ${datos.email}`)
+  }
+
+  const handleInput= (ev)=>{
+
+    setDatos({
+      ...datos,
+      [ev.target.name]:ev.target.value
+    })
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className="">
+      <form onSubmit={alerta}>
+        <input type="text" name="email" placeholder="email" value={datos.email} onChange={handleInput}/>
+        <input type="password" name="password" placeholder="password" value={datos.password} onChange={handleInput}/>
+        <Button type="submit" nombre="log in" handleFunction={()=>console.log("boton")}/>
+      </form>    
+    </div>
   )
 }
 
