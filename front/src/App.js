243263eea495849bef508a19067b28cbd640acf2
lautaroncoria <lautaro.coria@arbusta.net>
2022-09-05 15:21:42 -0300

    Se agregaron las rutas en App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import OrientedSignUpPage from './pages/OrientedSignUpPage';
+import Button from './components/Button';
+import Header from './components/Header';
+import OrientedList from './components/OrientedList';
+import Oriented from './components/Oriented';
 
 function App() {
   return (
-    <div>
+    <Routes>
+      <Route path={'/'} element={<HomePage />} />
+      <Route path={'/login'} element={<LoginPage />} />
+      <Route path={'/new-oriented'} element={<OrientedSignUpPage />} />
 
-    </div>
+      {/*  Las Rutas de abajo son sólo temporales por si necesitan ir viendo lo que crean */}
+      <Route path={'/button'} element={<Button />} />
+      <Route path={'/header'} element={<Header />} />
+      <Route path={'/oriented-list'} element={<OrientedList />} />
+      <Route path={'/oriented'} element={<Oriented />} />
+    </Routes>
   );
 }
 
