8a0bdd022a37658bccea2eacf8370984355adce9
GonzaloAlvrz <gonzalo.alvarez@arbusta.net>
2022-09-12 10:47:01 -0300

    header

--- front/src/components/HeaderAdmin.jsx
+++ front/src/components/HeaderAdmin.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ProfilePictureHeader from '../img/ProfilePictureHeader.svg'
 import ProfilePicture from '../components/ProfilePicture'
-import Menu from './Menu'
+
 
 
 function HeaderAdmin({Titulo}) {
   return (
-    <div className='flex flex-row'>
-          <Menu/>
         <div className='bg-blue w-full h-[112px] flex items-center'>
             <h1 className='text-2xl ml-[46px] text-white font-normal'>{/* {Titulo} */} Holaaaa</h1>
             <div className='ml-[992px]'><ProfilePicture picture={ProfilePictureHeader} alt="User"/></div>    
         </div>
-    </div>
   )
 }
 export default HeaderAdmin
--- front/src/components/Menu.jsx
+++ front/src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'react-router-dom'
 import Icon from '../img/vnegro.svg'
 import IconHome from '../img/IconHome.svg'
 import IconOrientados from '../img/IconOrientados.svg'
@@ -10,7 +11,7 @@ export default function Menu() {
    
         <div className='w-[234px] h-screen bg-white border-r border-bordergray'>
          <img className='ml-auto mr-auto mt-8 w-[166px] h-[41px] ' src={Icon} alt="" />
-         <div><h3 className='text-base mt-[67px] flex flex-row ml-8 font-normal '> <img className='mr-2' src={IconHome} alt="" />Inicio</h3></div>
+         <div><Link ><h3 className='text-base mt-[67px] flex flex-row ml-8 font-normal '> <img className='mr-2' src={IconHome} alt="" />Inicio</h3></Link></div>
          <div><h3 className='text-base mt-8 flex flex-row ml-8 font-normal '> <img className='mr-2' src={IconOrientados} alt="" />Orientados</h3></div>
          <div><h3 className='text-base mt-8 flex flex-row ml-8 font-normal '> <img className='mr-2' src={IconEventos} alt="" />Eventos</h3></div>
         </div>
