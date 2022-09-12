97462eee570d2610d6c019e241b96bb18ea65970
Guada Garcilazo <guadalupe.garcilazo@arbusta.net>
2022-09-12 12:06:27 -0300

    Nuevos cambios

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import OrientedList from "../components/OrientedList";
 import HeaderAdmin from '../components/HeaderAdmin';
+import Menu from '../components/Menu';
 
 function HomePage() {
 
@@ -10,6 +11,7 @@ function HomePage() {
 
 <div className="">
       <HeaderAdmin Titulo='¡Bienvenida, Susana!'/>
+      <Menu/>
       <div className="flex items-center my-3 ">
         <h4 className="h-16 w-48 bg- px-7 py-5 ml-[46px] mx-[750px] border-b-4 border-green">Nuevos orientados</h4>
         <Button  type="button" name="Ingresar orientado"/>
