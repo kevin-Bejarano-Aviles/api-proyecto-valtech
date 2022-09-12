152fc0275dc993d61362e0a07869e12036913354
lautaroncoria <lautaro.coria@arbusta.net>
2022-09-12 18:27:13 -0300

    Se logró traer los datos del orientado ingresado

--- front/src/pages/OrientedPage.jsx
+++ front/src/pages/OrientedPage.jsx
@@ -1,81 +1,112 @@
-import { Fragment } from 'react';
-import { Link, Outlet} from 'react-router-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import HeaderAdmin from '../components/HeaderAdmin';
+import Menu from '../components/Menu';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function Oriented() {
+  const params = useParams();
+  console.log(params);
+
+  const [user, setUser] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      let res = await axios('http://localhost:8000/admin/students');
+      const user = res.data.filter(item => item.dni === parseInt(params.id))[0];
+      setUser(user);
+    } catch (err) {
+      let message = err.response.statusText || 'Ocurrió un error';
+      console.log(message);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  },[]);
+
   return (
-    <Fragment>
-      <HeaderAdmin Titulo={<Outlet/>} />
-      <main className='ml-6'>
-        <section> {/* Información básica */}
-          <h2 className='my-4 text-xl font-bold'>01. Información básica</h2>
-          <div className='flex'>
-            <img
-              src='https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133352102-stock-illustration-default-placeholder-profile-icon.jpg'
-              alt='avatar'
-              className='mr-4 w-16 h-16'
-            />
-            <div className='mr-4'>
-              <h3 className='font-semibold text-lg'>Julian Martinez</h3>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>EMAIL</p>
-                <p className='font-semibold text-sm'>julian.martinez@gmail.com</p>
-              </div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>TELEFONO</p>
-                <p className='font-semibold text-sm'>1544806208</p>
-              </div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>PROGRAMA POR INICIAR</p>
-                <p className='font-semibold text-sm'>Orientación vocacional</p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section> {/* Datos personales */}
-          <h2 className='my-4 text-xl font-bold'>02. Datos personales</h2>
-          <div className='flex'>
-            <div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>NÚMERO DE DNI</p>
-                <p className='font-semibold text-sm'>28456387</p>
-              </div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>COLEGIO</p>
-                <p className='font-semibold text-sm'>Nuestra señora del Valle</p>
-              </div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>POR QUÉ SE ACERCÓ A NUESTRA INSTITUCIÓN</p>
-                <p className='font-semibold text-sm'>Necesira orientación para elegir una carrera.</p>
+    <div className='grid grid-cols-[234px_1fr] gap-0'>
+      <Menu />
+      <div>
+        <HeaderAdmin />
+        <main className='flex flex-col gap-10 ml-12 pb-12'>
+          <section className='mt-12'> {/* Información básica */}
+            <h2 className='mb-4 text-2xl font-bold'>01. Información básica</h2>
+            <div className='flex gap-4'>
+              <img
+                src='https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133352102-stock-illustration-default-placeholder-profile-icon.jpg'
+                alt='avatar'
+                className='w-[96px] h-[96px] rounded-full'
+              />
+              <div className='flex flex-col gap-2'>
+                <h3 className='text-2xl'>{user.fullName}</h3>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>EMAIL</p>
+                  <p>{user.email}</p>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>TELEFONO</p>
+                  <p>{user.phoneNumber}</p>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>PROGRAMA POR INICIAR</p>
+                  <p>{user.program}</p>
+                </div>
               </div>
             </div>
-            <div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>EDAD</p>
-                <p className='font-semibold text-sm'>17 años</p>
+          </section>
+          <section> {/* Datos personales */}
+            <h2 className='mb-4 text-2xl font-bold'>02. Datos personales</h2>
+            <div className='flex gap-4'>
+              <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>NÚMERO DE DNI</p>
+                  <p>{user.dni}</p>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>COLEGIO</p>
+                  <p>{user.school}</p>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>POR QUÉ SE ACERCÓ A NUESTRA INSTITUCIÓN</p>
+                  <p>{user.motive}</p>
+                </div>
               </div>
-              <div className='my-2'>
-                <p className='text-xs text-gray-400'>DOMICILIO</p>
-                <p className='font-semibold text-sm'>Av. Córdoba 2445 piso 6 dpto C, CABA</p>
+              <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>EDAD</p>
+                  <p>{user.age} años</p>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-xs text-gray-400'>DOMICILIO</p>
+                  <p>{user.address}</p>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-        <section className='mb-4'> {/* Crear usuario y contraseña */}
-          <h2 className='my-4 text-xl font-bold'>03. Crear usuario y contraseña</h2>
-          <div className='my-2'>
-            <p className='text-xs text-gray-400'>USUARIO</p>
-            <p className='font-semibold text-sm'>julian.martinez</p>
-          </div>
-          <div className='my-2'>
-            <p className='text-xs text-gray-400'>CONTRASEÑA</p>
-            <p className='font-semibold text-sm'>******</p>
-          </div>
-        </section>
-        <Button type='button' name='Asignar orientador/a' />
-      </main>
-    </Fragment>
+          </section>
+          <section className='mb-4'> {/* Crear usuario y contraseña */}
+            <h2 className='mb-4 text-2xl font-bold'>03. Crear usuario y contraseña</h2>
+            <div className='flex flex-col gap-2'>
+              <div className='flex flex-col gap-2'>
+                <p className='text-xs text-gray-400'>USUARIO</p>
+                <p>{user.user}</p>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <p className='text-xs text-gray-400'>CONTRASEÑA</p>
+                <p>••••••</p>
+              </div>
+            </div>
+          </section>
+          {/* <Link to={`/orientados/${user}/asignar-orientador`}>
+            <Button type='button' name='Asignar orientador/a' />
+          </Link> */}
+        </main>
+      </div>
+    </div>
   )
 }
 
