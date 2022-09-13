031a4f28fc4c68bf4731f612ecfec5d7c10597a9
lautaroncoria <lautaro.coria@arbusta.net>
2022-09-13 10:16:58 -0300

    Se agregó botón cerrar sesión

--- front/src/components/HeaderAdmin.jsx
+++ front/src/components/HeaderAdmin.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProfilePictureHeader from '../img/ProfilePictureHeader.svg'
 import ProfilePicture from '../components/ProfilePicture'
+import { Link } from 'react-router-dom'
 
 
 
@@ -10,7 +11,11 @@ function HeaderAdmin({Titulo}) {
 
             <h1 className='text-2xl ml-[46px] text-white font-normal'> {Titulo}</h1>
 
-            <div className='ml-[992px]'><ProfilePicture picture={ProfilePictureHeader} alt="User"/></div>    
+            <div className='ml-[992px]'><ProfilePicture picture={ProfilePictureHeader} alt="User"/></div>
+
+            <Link to='/login'>
+              <button className='px-2 py-1 bg-gray-300 rounded-md'>Cerrar sesión</button>
+            </Link>
         </div>
   )
 }
