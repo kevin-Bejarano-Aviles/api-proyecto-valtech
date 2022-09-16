b70ed5961ad4427aad4e13caa4d6ae3678236786
Guada Garcilazo <guadalupe.garcilazo@arbusta.net>
2022-09-16 10:17:59 -0300

    Cambios

--- front/src/components/Oriented.jsx
+++ front/src/components/Oriented.jsx
@@ -1,7 +1,7 @@
 function Oriented({ info }) {
   return (
     <div className='w-60 h-42 inline-block list-none py-2 mx-2 my-3 px-2 border-solid border-2 rounded-md border-slate-200'>
-    {/* <img className='w-24 h-24 rounded-full' src={require(`../img/students/${info.avatar}`)} alt='image'/> */}
+    <img className='w-24 h-24 rounded-full' src={require(`../img/students/${info.avatar}`)} alt=''/>
     <h3 className='font-medium text-base'>{info.fullName}</h3>
     <h4 className='text-sm text-blue'>{info.school}</h4>
   </div>
