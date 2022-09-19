dcab7fd9871eecdd223911ee3ba2467682654627
lautaroncoria <lautaro.coria@arbusta.net>
2022-09-19 10:46:02 -0300

    Se le dio formato al archivo

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -18,13 +18,10 @@ module.exports = {
       },
       screens: {
         'mobile': '360px',
-        
         'tablet': '640px',
         // => @media (min-width: 640px) { ... }
-  
         'laptop': '1024px',
         // => @media (min-width: 1024px) { ... }
-  
         'desktop': '1280px',
         // => @media (min-width: 1280px) { ... }
       }
