456a823c711eb9f05a32fec9e555c7bd8b65090d
lautaroncoria <lautaro.coria@arbusta.net>
2022-09-19 12:17:38 -0300

    Si se hace click sobre un orientado en la lista de orientados, se redirige a la vista de detalle del orientado seleccionado

--- front/src/components/Oriented.jsx
+++ front/src/components/Oriented.jsx
@@ -1,14 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 function Oriented({ info }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="w-[940px] h-[100px] flex">
+    <div className='w-[940px] h-[100px] flex cursor-pointer' onClick={() => {navigate(`/orientados/${info.id}`)}}>
       <div className='w-60 h-42 list-none py-2 mx-2 my-3 px-2 border-solid border-2 rounded-md border-slate-200 flex'>
         <img className='w-16 h-16 rounded-full ml-[-5px] mt-[-4px] mx-1' src={require(`../img/students/${info.avatar}`)} alt=''/>
-       <div className="">
-        <h3 className='font-medium text-base'>{info.fullName}</h3>
-        <h4 className='text-sm text-blue'>{info.school}</h4>
-       </div>
-     </div>
-  </div>
+        <div className=''>
+          <h3 className='font-medium text-base'>{info.fullName}</h3>
+          <h4 className='text-sm text-blue'>{info.school}</h4>
+        </div>
+      </div>
+    </div>
   )
 }
 
