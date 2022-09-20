c20f4c7e67a076b8bbb0f1f2ca40bca63c5febc1
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-09-20 10:34:46 -0300

    header y menuPublic Responsives

--- front/src/pages/EventsPage.jsx
+++ front/src/pages/EventsPage.jsx
@@ -1,24 +1,31 @@
-import Button from "../components/Button";
-import {  useNavigate } from "react-router-dom";
+import Button from '../components/Button';
+
+import {  useNavigate } from 'react-router-dom';
+import Icon_Search from '../img/Icon_search.svg'
 
 function EventsPage() {
     let navigate = useNavigate();
 
     return ( 
         <>
-        <div className="flex justify-between">
-            <div className="flex">
-                <h1 className="absolute z-1 text-2xl font-semibold">Todos los eventos</h1>
-                <div className="h-3.5 w-56  bg-backgroundGray"></div>
+        <div>
+            <div className='flex justify-between'>
+                <div className='relative h-8 w-56 '>
+                    <h1 className='text-blue absolute w-56 z-20 text-2xl font-normal'>Todos los eventos</h1>
+                    <div className='absolute z-10 inset-x-0 bottom-0 h-3.5 w-56  bg-backgroundGray'></div>
+                </div>
+                <Button
+                    type='button'
+                    name='Agendar evento'
+                    handleFunction={()=>{navigate('/eventos/crear-evento')}}
+                />
             </div>
-            <Button
-                type="button"
-                name="Agendar evento"
-                handleFunction={()=>{navigate("/eventos/crear-evento")}}
-            />
-        </div>
-        <div class="">
-            
+            <div>
+                <h1 className='text-blue text-xl font-semibold'>Buscar eventos de un orientado</h1>
+               
+
+            </div>
+            <div></div>
         </div>
         </>
      );
