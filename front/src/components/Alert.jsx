1c350e197d3388c0da6fbbf7620e734cf2a5abaf
lautaroncoria <lautaro.coria@arbusta.net>
2022-09-22 00:20:50 -0300

    Componente Alert ahora es responsivo

--- front/src/components/Alert.jsx
+++ front/src/components/Alert.jsx
@@ -3,7 +3,7 @@ import iconClose from '../img/icon-close.svg';
 
 function Alert({ message, onclick }) {
   return (
-    <div className='fixed bottom-5 flex justify-between p-4 w-[768px] font-bold text-sm text-white bg-blue rounded-lg'>
+    <div className='mobile:bottom-3 mobile:w-[300px] tablet:bottom-5 tablet:w-[500px] laptop:w-[600px] desktop:w-[768px] fixed flex justify-between p-4 font-bold text-sm text-white bg-blue rounded-lg'>
       <div className='flex gap-6'>
         <img src={iconCheck} alt='' />
         {message}
