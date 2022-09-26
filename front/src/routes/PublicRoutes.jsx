e0e368ee7ae7a0b182ff5715b4a4690ac7cbf0ed
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-09-26 10:22:14 -0300

    reedireccion de rutas

--- front/src/routes/PublicRoutes.jsx
+++ front/src/routes/PublicRoutes.jsx
@@ -4,7 +4,7 @@ import Context from '../context/Context';
 
 function PublicRoutes({children}) {
     const {estado}=useContext(Context)
-    return ( (!estado)?children : <Navigate to='/inicio'/>  
+    return ( (!estado)?children : <Navigate to='/'/>  
        
      );
 }
