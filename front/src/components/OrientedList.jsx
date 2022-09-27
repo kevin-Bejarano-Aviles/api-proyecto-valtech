9be51c28fa2ba0c121ea6c1d04779a7ca60c4b89
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-09-27 10:18:23 -0300

    vista boton en caso de que no se vea

--- front/src/components/OrientedList.jsx
+++ front/src/components/OrientedList.jsx
@@ -9,9 +9,9 @@ function OrientedList() {
 
   const {deslogearme} = useContext(Context);
 
-  const [cantUsers,setCantusers]=useState(0);
 
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [cantUsers,setCantusers]=useState(0);
 
   const [seeMore, setSeeMore] = useState(false);
   
@@ -26,20 +26,20 @@ function OrientedList() {
     } catch (error) {
       login();
       console.log(error)
-
     }
   };
 
   useEffect(() => {
     getAll();
   },[]);
-
+  console.log(users.length);
+  console.log('no hay orientados'+ users);
 
   return (
     <div>
       <ul className='grid mobile:grid-cols-1 lap_tablet:grid-cols-2 desktop:grid-cols-3 gap-4 mt-8 mb-2 max-w-max'>
         {
-          !users ? 'No hay orientados' : (
+          users.length===0 ? 'No hay orientados' : (
             users.map((user, index) => (
               index <= 8 ? <Oriented info={user}/> : seeMore ? <Oriented info={user}/> : ''
             ))
@@ -52,12 +52,11 @@ function OrientedList() {
           className='ml-2 h-4 max-w-max underline cursor-pointer'
           onClick={() => setSeeMore(!seeMore)}
         >
-          {seeMore ? 'Ver menos orientados' : 'Ver más orientados'
-          
+          {
+            users.length===0 ? '' : (seeMore ? <p>Ver menos orientados</p> : <p>Ver mas orientados</p>)
           }
         </span>
-        <div className='relative -z-10 bottom-[11px] h-3 bg-yellow'></div>
-      
+        <div className={users.length===0 ? 'hidden' : 'relative -z-10 bottom-[11px] h-3 bg-yellow'}></div>
       </div>
       }
     </div>
