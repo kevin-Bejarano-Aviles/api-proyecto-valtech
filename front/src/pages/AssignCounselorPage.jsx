c72dcfe938d3f02203d66abdd0e8f93d2f454e7c
GonzaloAlvrz <gonzalo.alvarez@arbusta.net>
2022-10-04 17:19:56 -0300

    Modificar orientador

--- front/src/pages/AssignCounselorPage.jsx
+++ front/src/pages/AssignCounselorPage.jsx
@@ -1,7 +1,6 @@
 import { React, useEffect, useState }  from "react";
 import Menu from '../components/Menu';
 import HeaderAdmin from '../components/HeaderAdmin';
-import AssingComponente from '../components/AssignComponent'
 import {  useNavigate } from 'react-router-dom';
 import axios from 'axios'; 
 import Button from '../components/Button'
@@ -9,9 +8,9 @@ import { useForm } from 'react-hook-form';
 import { useParams, NavLink } from 'react-router-dom';
 import Alert from '../components/Alert'
 
-/* import AssignOriented from "../components/AssignOriented"; */
 
 function AssignCounselorPage() {
+  const [user, setUser] = useState([]);
   const [oriented, setOriented] = useState([]);
   const [isVisible, setIsVisible] = useState(false)
   const [selectOption, setSelectOption] = useState('')
@@ -31,6 +30,19 @@ function AssignCounselorPage() {
 
   const [ocultar, setOcultar] = useState (true)
 
+  const [ocultarCard, setOcultarCard] = useState(false)
+  // const clickOcultarCard = () => {
+  //   setOcultarCard (!ocultarCard);
+  // }
+
+   const [mostrar, setMostrar] = useState(false)
+   const clickMostrar = () => {
+     setMostrar (!mostrar);
+ }
+
+
+
+
   const params = useParams();
   const navigate = useNavigate();
 
@@ -42,10 +54,39 @@ function AssignCounselorPage() {
     }
   });
 
+ /* Traigo los datos del orietado*/
+
+ const getAllStudents = async () => {
+  try {
+    let res = await axios.get(`http://localhost:8000/admin/students/${params.id}`, { withCredentials: true });
+    setUser(res.data.student);
+    // console.log(res.data.student);
+   } catch (err) {
+    navigate('error');
+    let message = err.response.statusText || 'Ocurrió un error';
+    console.log(message);
+   }
+  };
+
+   useEffect(() => {
+     getAllStudents();
+   },[]);
+
+
+
+
+
+
+
+
+
+  /* Traigo los datos de los orietadores  */
+
   const getAllOriented = async () => {
     try {
       let Orientador = await axios.get(`http://localhost:8000/admin/advisers`, { withCredentials: true });
       setOriented(Orientador.data.advisers);
+      // console.log(Orientador.data.advisers)
       
     } catch (Orientador) {
       navigate('error');
@@ -76,6 +117,7 @@ function AssignCounselorPage() {
           withCredentials: true,
           data: {...data}
       };
+      console.log(options.data);
       const response = await axios(`http://localhost:8000/admin/assignAdviser/${params.id}`, options);
       console.log(response.data); 
       
@@ -97,87 +139,163 @@ function AssignCounselorPage() {
          <section className=" mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row">
          <h2 className="text-2xl text-blue ml-[46px]  mt-8 ">Asignación de Orientador Referente</h2>
       <div className=" ">
-          <AssingComponente/>
+
+      <div className="py-4 flex flex-row  mt-4 ml-[46px] mr-6 mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center  border-2 border-graybackground rounded-lg ">
+      <div className='w-[178px] h-[178px] flex justify-center items-center border-r-[1px] border-bordergray'><img className='w-[140px] h-[140px] rounded-full' src={user.avatar ? require(`../img/students/${user.avatar}`) : 'https://i.imgur.com/b08hxPY.png'} alt='avatar'/></div>
+      <div className='laptop:max-w-[823px] h-[178px] ml-8 mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center'>
+      <div className=' mobile:mx-auto lap_tablet:mx-auto  tablet:mx-auto'>
+        <h2 className='text-2xl font-normal ml-6 '>{user.fullName}</h2>
+        <h4 className='text-[15px] leading-[22px]  ml-6 text-lightgray'>Orientado</h4>
       </div>
-         </section>
-         <seccion>
-         <h2 className="text-2xl text-blue ml-[46px]  mt-8">Selección de un Orientador Referente</h2>
-         <h4 className="text-[14px] leading-4 text-blue ml-[46px]  mt-4">Referente</h4>
-         
-        <form  onSubmit={handleSubmit(onSubmit)}>
-        <select
-         {...register('idAdviser')}
-        onChange={(a)=>{
-          const valueOption = a.target.value;
-          setSelectOption(valueOption);
-        }}
-/*         onChange={(a)=>{
-          const valueOption = a.target.value;
-          setSelectOption(valueOption);
-        }} */
-        onClick={()=>setIsVisible(true)}
-        
-        className="mt-2 ml-10 mobile:max-w-[320px] tablet:max-w-[320px] pl-1 pr-2 h-10 rounded-lg text-gray-400  border-2"   >
-        <option selected hidden value=''>Seleccionar orientador</option>
-            {oriented.map (elemento=>(
-             <option key={elemento.id} value={[`${elemento.id}`, ` ${elemento.email}`]} >{elemento.fullName}</option>
-            ))}
-        </select>
-          
-      
-
-        <div  className={isVisible ? "block" : "hidden"}>
-  
-        {/* {oriented.map(info=>( */}
-        <div className="py-4 flex flex-row  mt-4 mb-6 ml-[46px] mr-6 mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center  border-2 border-graybackground rounded-lg ">
-        <div className='w-[178px] h-[178px] flex justify-center items-center border-r-[1px] border-bordergray'><img className='w-[140px] h-[140px] rounded-full' src={'' ? require(`../img/adviser/${''}`) : 'https://i.imgur.com/b08hxPY.png'} alt={selectOption}/></div>
-        <div className='laptop:max-w-[823px] h-[178px] ml-8  mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center'>
-        <div className=' mobile:mx-auto lap_tablet:mx-auto  tablet:mx-auto'>
-        <h2 className='text-2xl font-normal ml-6 '></h2>
-        <h4 className='text-[15px] leading-[22px]  ml-6 text-lightgray'>Orientador</h4>
-        </div>
-        <div className='flex flex-row '>
-        <div className='w-1/2 px-6 '>
+      <div className='flex flex-row '>
+      <div className='w-1/2 px-6 '>
         <h5 className=' text-xs text-lightgray'>mail</h5>
-        <p className=' text-[16pxpx] leading-[26px] text-blue '>{selectOption}</p>
-        </div>
-        <div className='w-1/2 px-6 '>
-        <h5 className=' text-xs text-lightgray'>Telefono</h5>
-        <p className=' text-[16pxpx] leading-[26px] text-blue '>Taller de matematicas</p>
-        </div>
-        </div>
-        </div>
-        </div>
-      {/*   ))} */}
+        <p className=' text-[16pxpx] leading-[26px] text-blue'>{user.email}</p>
+        <h5 className=' text-xs text-lightgray'>Colegio</h5>
+        <p className=' text-[16pxpx] leading-[26px] text-blue'>{user.school}</p>
+      </div>
+      <div className='w-1/2 px-6 '>
+      <h5 className=' text-xs text-lightgray'>Telefono</h5>
+      <p className=' text-[16pxpx] leading-[26px] text-blue'>{user.phoneNumber}</p>
+      <h5 className=' text-xs text-lightgray'>Programa</h5>
+      <p className=' text-[16pxpx] leading-[26px] text-blue'> {user.program}</p>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+        </section>
 
-        </div>
 
-        <div className="ml-10 mt-16 mb-8 flex flex-row "  onClick={()=>setViewButton(false)}>
-        
-        <div className={alert ? "hidden" : "block"}  onClick={clickAlert}>
-        <Button type='submit' name='Asignar orientador/a' disabled={isEmpty}  />
-        </div>
-        <div className="hidden">.</div>
-        </div>
-        <div className={viewButton ? "hidden" : "block"}>
-          <div className="flex flex-row ml-10 relative bottom-10 items-center">
-          <div  onClick={clickAlertm}>
-          <Button type='submit' name='Modificar orientador/a' disabled={isEmpty}  /> 
+        <section>
+        <h2 className="text-2xl text-blue ml-[46px]  mt-8">Selección de un Orientador Referente</h2> 
+        <div>
+          <div>  
+            <form  onSubmit={handleSubmit(onSubmit)} id="1">
+              <div className={ocultarCard || (user.adviser === null) ? "block" : "hidden"}>
+                <div>
+                <label className='text-[14px] leading-4 text-blue ml-[46px]  mt-4'>Referente</label>
+                </div>
+                <select
+                  {...register('idAdviser')}
+                  onChange={(a)=>{
+                    const valueOption = a.target.value;
+                    setSelectOption(valueOption);
+                  }}
+    
+                  onClick={()=>setIsVisible(true)}
+                  className='mt-2 ml-10 mobile:max-w-[320px] tablet:max-w-[320px] pl-1 pr-2 h-10 rounded-lg text-gray-400  border-2'   >
+                    <option selected hidden value=''>Seleccionar orientador</option>
+                    {oriented.map (elemento=>(
+                      <option key={elemento.id} value={[`${elemento.id}`, ` ${elemento.email}`]} >
+                        {elemento.fullName}
+                      </option>
+                    ))}
+                </select>
+              </div>
+
+              <div  className={isVisible ? "block" : "hidden"}>
+                
+                <div className="py-4 flex flex-row  mt-4 mb-6 ml-[46px] mr-6 mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center  border-2 border-graybackground rounded-lg ">
+                  <div className='w-[178px] h-[178px] flex justify-center items-center border-r-[1px] border-bordergray'>
+                    <img 
+                      className='w-[140px] h-[140px] rounded-full'
+                      src={'' ? require(`../img/adviser/${''}`) : 'https://i.imgur.com/b08hxPY.png'}
+                      alt={selectOption}/>
+                  </div>
+                  <div className='laptop:max-w-[823px] h-[178px] ml-8  mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center'>
+                    <div className=' mobile:mx-auto lap_tablet:mx-auto  tablet:mx-auto'>
+                      <h2 className='text-2xl font-normal ml-6 '></h2>
+                      <h4 className='text-[15px] leading-[22px]  ml-6 text-lightgray'>Orientador</h4>
+                    </div>
+                    <div className='flex flex-row '>
+                      <div className='w-1/2 px-6 '>
+                        <h5 className=' text-xs text-lightgray'>mail</h5>
+                        <p className=' text-[16pxpx] leading-[26px] text-blue '>{selectOption}</p>
+                      </div>
+                      <div className='w-1/2 px-6 '>
+                        <h5 className=' text-xs text-lightgray'>Telefono</h5>
+                        <p className=' text-[16pxpx] leading-[26px] text-blue '>Taller de matematicas</p>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
+              </div>
+              <div className="ml-10 mt-16 mb-8 flex flex-row "  onClick={()=>setViewButton(false)}>
+                <div className={`${alert ? "hidden" : "block"} ${ocultarCard || (user.adviser === null) ? "block" : "hidden"} `}  onClick={clickAlert}>
+                  <Button type='submit' name='Asignar orientador/a' disabled={isEmpty}  />
+                </div>
+                <div className="hidden">.</div>
+              </div> 
+              <div className={viewButton ? "hidden" : "block"}>
+                <div className="flex flex-row ml-10 relative bottom-10 items-center">
+                  <div>
+                  <div  onClick={clickAlertm}>
+                    <Button type='submit'  name='Modificar orientador/a' disabled={isEmpty}  /> 
+                  </div></div>
+                  <div className="ml-4 underline">
+                    <NavLink to={`/orientados/${params.id}`}>Volver</NavLink>
+                  </div>
+                </div>
+              </div>
+            </form> 
           </div>
-          <div className="ml-4 underline">
+        </div>
+          
+
+        <div  className={ocultarCard ? "hidden" : "block"}>
+        <div className={user.adviser !==null ? "block" : "hidden"}>
+          
+           <div className="py-4 flex flex-row relative bottom-10 ml-[46px] mr-6 mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center  border-2 border-graybackground rounded-lg ">
+            <div className='w-[178px] h-[178px] flex justify-center items-center border-r-[1px] border-bordergray'><img className='w-[140px] h-[140px] rounded-full' src={'' ? require(`../img/adviser/${''}`) : 'https://i.imgur.com/b08hxPY.png'} alt={selectOption}/></div>
+            <div className='laptop:max-w-[823px] h-[178px] ml-8  mobile:flex-col lap_tablet:flex-col  tablet:flex-col laptop:flex-row items-center'>
+            <div className=' mobile:mx-auto lap_tablet:mx-auto  tablet:mx-auto'>
+            <h2 className='text-2xl font-normal ml-6 '></h2>
+            <h4 className='text-[15px] leading-[22px]  ml-6 text-lightgray'>Orientador</h4>
+            </div>
+            <div className='flex flex-row '>
+            <div className='w-1/2 px-6 '>
+            <h5 className=' text-xs text-lightgray'>mail</h5>
+            <p className=' text-[16pxpx] leading-[26px] text-blue'>{user.Adviser && user.Adviser.email}</p>
+            </div>
+            <div className='w-1/2 px-6 '>
+            <h5 className=' text-xs text-lightgray'>Telefono</h5>
+            {console.log(user.Adviser && user.Adviser.email)}
+            <p className=' text-[16pxpx] leading-[26px] text-blue'>{user.Adviser && user.Adviser.phoneNumber}</p>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+           
+           <div className={user.adviser !==null ? "block" : "hidden"} >
+           <div className="flex flex-row ml-10 mt-16 relative bottom-10 items-center">
+
+           <Button type='button' handleFunction={() => setOcultarCard(true)}  name='Modificar orientador/a'/>
+
+           <div className="ml-4 underline">
             <NavLink to={`/orientados/${params.id}`}>Volver</NavLink>
+           </div>
+           </div>
           </div>
           </div>
-        </div>
-        
-        </form>
+
+
+  
+
+
         <div className="mt-24 ml-10">
         <div className={!ocultar ? "hidden" : "block"}> 
         {alert ? <Alert message='El orientado ya fue asignado a su referente.' onclick={() => setOcultar(false)}/> : ''}
         </div>
+{/*         <div>
         {alertm ? <Alert message='El orientado fue modificado.' onclick={() => clickAlertm(false)}/> : ''}
+        </div> */}
         </div>
-        </seccion>
+        </section>
+         
+
+
       </main>
       </div>
 
@@ -186,4 +304,5 @@ function AssignCounselorPage() {
 }
 
 export default AssignCounselorPage; 
+
 
