f54b692331029b3ae7d5027b45070f52a186179f
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-10-11 13:27:11 -0300

    perf(web):se cambio el manejo de errores de login con formik y yup

--- front/src/pages/PublicPages/Login/LoginPage.jsx
+++ front/src/pages/PublicPages/Login/LoginPage.jsx
@@ -1,120 +1,74 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import Button from '../../PrivatePages/sharedPrivateComponents/button/Button';
+import { useContext } from 'react';
+import { Formik, useFormik } from 'formik';
+import * as Yup from 'yup';
 import axios from 'axios'
+
+import Button from '../../PrivatePages/sharedPrivateComponents/button/Button';
 
 import warningImg from '../../../assets/icons/icon_warning.svg'
 import ilustration from '../../../assets/ilustration/login/ilustration.svg';
 import title from '../../../assets/logo/vnegro.svg'
-import { useContext } from 'react';
 import Context from '../../../context/Context';
 
 function LoginPage() {
-  // const [band,useState]=useState(0)
-  const [message,setMessage]=useState(null)
 
-  const [bandEmail,setBandEmail]=useState(0)
-  const [bandPass,setBandPass]=useState(0)
-    const [bandButton,setBandButton]=useState(0)
-
-  //state variables so that the field is not empty
-  const [errorPassword,SetErrorPassword]=useState(false);
-  const [errorEmail,SetErrorEmail]=useState(false);
-
-    //state variables showing error message
-  const [errorMessage,SetErrorMessage]=useState(false);
-
-  const [data,setData] = useState({
-    email:'',
-    pass:''
-  });
+  const [errorMessage,setErrorrMessage]=useState('')
 
   const url=process.env.REACT_APP_API_URL
   const baseUrl =`${url}/admin/adminLogin`
   const navigate = useNavigate();
-    const {logIn} = useContext(Context);
-    const login = ()=>{
-      logIn()
-      navigate('/login',{replace:true})
+  const {logIn} = useContext(Context);
+  const login = ()=>{
+    logIn()
+    navigate('/login',{replace:true})
   }
 
-  const postData = async (data) => {
-    await axios.post(baseUrl,{
-      email:data.email,
-      pass:data.pass,
-    },{withCredentials:true})
-    .then(response=>{
-        login();
-        // console.log(response.data.adminLog);
-        localStorage.setItem('admin', JSON.stringify(response.data.adminLog));
-      })
-    .catch(error => {
-      setMessage(error.response.data.message);
-      SetErrorMessage(true)
-
-    })
-
-    }
-
-  //I need to validate the user and control the messages
-  const alerta = async (e) =>{
-    e.preventDefault();
-    postData(data);
-  }
-
-  const handleInput= (ev)=>{
-    setData({
-      ...data,
-      [ev.target.name]:ev.target.value
-    })
-    if(ev.target.name==='email'){
-      setBandEmail(1);
-      if(bandPass===1){
-        setBandButton(1);
+  const dataForm=useFormik({
+      initialValues:{
+        email:'',
+        pass:''
+      },
+      validationSchema:Yup.object({
+        email:Yup.string().email('Debe ser un email valido').required('El campo email no debe estar vacio'),
+        pass:Yup.string().required('El campo contraseña no debe estar vacio'),
+      }),
+      onSubmit:async (data)=>{
+        await axios.post(baseUrl,{
+              email:data.email,
+              pass:data.pass,
+            },{withCredentials:true})
+            .then(response=>{
+              login();
+              localStorage.setItem('admin', JSON.stringify(response.data.adminLog));
+            })
+            .catch(error => {
+              setErrorrMessage(error.response.data.message)
+            })
       }
     }
-    else if(ev.target.name==='pass'){
-      setBandPass(1);
-      if(bandEmail===1){
-        setBandButton(1)
-      }
-    }
-  }
+  )
 
-  // check that the field is not empty
-  const setErrors=()=>{
-    
-    if(data.pass.length<1 && !errorPassword && bandPass===1){
-      SetErrorPassword(true)
-    }
-    else if(data.pass.length>1 && errorPassword){
-      SetErrorPassword(false)
-    }
-    if(data.email.length<1 && !errorEmail && bandEmail===1){
-      SetErrorEmail(true)
-    }
-    else if(data.email.length>1 && errorEmail){
-      SetErrorEmail(false)
-    }
-  }
 
-  useEffect(()=>{
-    SetErrorPassword(false)
-      SetErrorEmail(false)
-  },[])
 
-  useEffect(()=>{
-    setErrors();
-  },[data])
- 
-  useEffect(()=>{
-    if(errorMessage){
-      const timer=setTimeout(()=>{
-        SetErrorMessage(false)
-      },8000);
-      return ()=> clearTimeout(timer)
-    }
-  },[errorMessage])
+  // const postData = async (data) => {
+  //   await axios.post(baseUrl,{
+  //     email:data.email,
+  //     pass:data.pass,
+  //   },{withCredentials:true})
+  //   .then(response=>{
+  //       login();
+  //       localStorage.setItem('admin', JSON.stringify(response.data.adminLog));
+  //     })
+  //   .catch(error => {
+  //     setMessage(error.response.data.message);
+  //     SetErrorMessage(true)
+
+  //   })
+
+  //   }
+
   const deskTopviewIlustration='laptop:static laptop:bg-graybackground laptop:h-screen laptop:w-3/5 laptop:flex laptop:flex-col laptop:justify-center'
   const deskTopviewForm='laptop:drop-shadow-none laptop:backdrop-blur-none laptop:bg-transparent laptop:flex laptop:flex-col laptop:justify-center laptop:items-start laptop:h-screen laptop:w-4/5 laptop:ml-20'
 
@@ -128,43 +82,52 @@ function LoginPage() {
   return (
     <div className={`flex flex-col tablet:flex-row`}>
       
-      <div className={`fixed z-0 left-0 right-0 top-24 bottom-0${deskTopviewIlustration} ${tabletviewIlustration}`}>
+      <div className={`fixed z-0 left-0 right-0 top-24 bottom-0 ${deskTopviewIlustration} ${tabletviewIlustration}`}>
         <img className='w-32 laptop:w-48 ' src={title} alt='v-camp'/>
         <img className='w-full tablet:w-full laptop:w-full' src={ilustration} alt='ilustracion'/>
       </div>
-      {/* start form */}
 
-      <form className={`flex flex-col items-center h-screen justify-center	z-0 bg-white bg-opacity-40 backdrop-blur-md rounded drop-shadow-lg ${deskTopviewForm} ${tabletviewsForm}`} onSubmit={alerta}>
+      <form className={`flex flex-col items-center h-screen justify-center	z-0 bg-white bg-opacity-40 backdrop-blur-md rounded drop-shadow-lg ${deskTopviewForm} ${tabletviewsForm}`} onSubmit={dataForm.handleSubmit}>
         <h2 className='text-3xl	font-bold mb-8 text-zinc-600'>Ingresá a tu portal</h2>
         <label className='flex flex-col mt-2 mb-5'>
           <p className='font-bold desktop:font-normal
  tablet:font-normal text-zinc-600 text-lg'>Email</p>
-          <input className={`${inputDesktopView} ${inputMobileView}`} type='text' name='email' placeholder='email' value={data.email} onChange={handleInput}/>
+          <input className={`${inputDesktopView} ${inputMobileView}`} type='text' name='email' placeholder='email'  onChange={dataForm.handleChange}/>
         </label>
-        <div className={`${!errorEmail ? 'hidden' : 'flex'} text-red-500	`}>
-          <img className='mr-2' src={warningImg} alt=''/>
-          <p className='font-bold desktop:font-normal tablet:font-normal'>El campo email no debe estar vacio</p>
-        </div>
+
+        {
+          dataForm.errors.email &&
+          <div className={`flex text-red-500	`}>
+            <img className='mr-2' src={warningImg} alt=''/>
+            <p className='font-bold desktop:font-normal tablet:font-normal'>{dataForm.errors.email}</p>
+          </div>
+        }
         
         <label className='flex flex-col mt-2 mb-5'>
         <p className='font-bold desktop:font-normal tablet:font-normal text-zinc-600 text-lg'>Contraseña</p>
-          <input className={`${inputDesktopView} ${inputMobileView}`} type='password' name='pass' placeholder='password' value={data.password} onChange={handleInput}/>
+          <input className={`${inputDesktopView} ${inputMobileView}`} type='password' name='pass' placeholder='password'  onChange={dataForm.handleChange}/>
         </label>
 
-        <div className={`${!errorPassword ? 'hidden' : 'flex '} text-red-500 `}>
-        <img className='mr-2' src={warningImg} alt=''/>
-          <p className='font-bold desktop:font-normal
- tablet:font-normal'>El campo contraseña no debe estar vacio</p>
-        </div>
-        
-        <div className={`${!errorMessage ? 'hidden' : 'flex'} text-red-500`}>
+        {
+          dataForm.errors.pass && 
+          <div className={`flex text-red-500 `}>
             <img className='mr-2' src={warningImg} alt=''/>
-            <p className='font-bold 
- tablet:font-normal'>{message ? message : null}</p>
+            <p className='font-bold desktop:font-normal tablet:font-normal'>{dataForm.errors.pass}</p>
+          </div>
+
+        }
+        
+        {
+            errorMessage &&
+            <div className={`flex text-red-500`}>
+            <img className='mr-2' src={warningImg} alt=''/>
+            <p className='font-bold tablet:font-normal'>{errorMessage}</p>
         </div>
+        }
+        {/*   */}
         
         <div className='mt-5'>
-          <Button type='submit' name='log in' disabled={((!errorEmail && !errorPassword) && bandButton===1) ? false : true}/>        
+          <Button type='submit' name='log in' />        
         </div>
       </form>    
     </div>
