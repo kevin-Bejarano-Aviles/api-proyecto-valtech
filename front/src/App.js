cb1d44fcd3d51755b46129b6b1a34f48a8f3e10f
guadagarcilazo <guadalupe.garcilazo@arbusta.net>
2022-10-27 10:32:57 -0300

    Formateo de codigo + correcciones de errores Eslint

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-
 import RoutersElement from './routes/RoutersElement';
 import Provider from './context/Provider';
 
-
 function App() {
   return (
-
     <Provider>
-      <RoutersElement/>
+      <RoutersElement />
     </Provider>
-
   );
 }
 
--- front/src/routes/PublicRoutes.jsx
+++ front/src/routes/PublicRoutes.jsx
@@ -2,11 +2,9 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import Context from '../context/Context';
 
-function PublicRoutes({children}) {
-    const {state}=useContext(Context)
-    return ( (!state)?children : <Navigate to='/inicio'/>  
-       
-     );
+function PublicRoutes({ children }) {
+  const { state } = useContext(Context);
+  return !state ? children : <Navigate to='/inicio' />;
 }
 
 export default PublicRoutes;
