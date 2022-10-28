e20e5a75724ca753cb9260d6c0324539f916cd4e
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-10-28 14:11:20 -0300

    cambios en componente select de form dar de alta orientado

--- front/src/pages/PrivatePages/hooks/useGet.jsx
+++ front/src/pages/PrivatePages/hooks/useGet.jsx
@@ -3,166 +3,158 @@ import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Context from '../../../context/Context';
 
+function useGet() {
+  const url = process.env.REACT_APP_API_URL;
+  const baseUrl = `${url}/admin`;
+  const token = localStorage.getItem('token');
+  const { logOut } = useContext(Context);
 
-function useGet(){
+  const [studentList, setStudentList] = useState([]);
+  const [eventList, setEventsList] = useState([]);
+  const [adviserList, setAdviserList] = useState([]);
+  const [studentDetail, setStudentDetail] = useState();
+  const [totalPagesEvent, seTotalPagesEvent] = useState(0);
+  const [totalEventsGet, setTotalEventsGet] = useState();
+  const [totalEvents, setTotalEvents] = useState();
 
-    const url=process.env.REACT_APP_API_URL;
-    const baseUrl =`${url}/admin`;
-    const token=localStorage.getItem('token');
-    const { logOut } = useContext(Context);
+  const navigate = useNavigate();
 
-    const [studentList,setStudentList]=useState([]);
-    const [eventList,setEventsList]=useState([]);
-    const [adviserList,setAdviserList]=useState([]);
-    const [studentDetail, setStudentDetail] = useState();
-    const [totalPagesEvent,seTotalPagesEvent]=useState(0);
-    const [totalEventsGet,setTotalEventsGet]=useState();
-    const [totalEvents,setTotalEvents]=useState();
+  const LogOut = () => {
+    logOut();
+    navigate('/login', { replace: true });
+  };
 
-    const navigate = useNavigate();
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'x-token': `Bearer ${token}`,
+    },
+  };
 
-    const LogOut = () => {
-        logOut();
-        navigate('/login', { replace: true })
+  const calculateTotalPages = (totalEvent) => {
+    let totalPages = 0;
+    let restEvents = 0;
+    let total = totalEvent;
+    while (total > 10) {
+      if (total > 10) {
+        totalPages += 1;
+      }
+      total -= 10;
+      restEvents = totalPages;
     }
-
-    const options= {
-        method: 'GET',
-        headers: { 'Content-Type': 'multipart/form-data'
-        ,'x-token':`Bearer ${token}`},
-    };
-
-    const calculateTotalPages = (totalEvent)=>{
-        let totalPages=0
-        let restEvents=0
-        let total=totalEvent;
-        while(total>10){
-          if(total>10){
-            totalPages+=1
-          }
-          total-=10;
-          restEvents=totalPages;
-        }
-        if(restEvents===0 && total>0){
-          totalPages += 1;
-        }
-        else if(restEvents>0){
-          totalPages += 1;
-        }
-        seTotalPagesEvent(totalPages);
+    if (restEvents === 0 && total > 0) {
+      totalPages += 1;
+    } else if (restEvents > 0) {
+      totalPages += 1;
     }
-    
-    const getAllStudents = async () => {
-        try{
-            const response = await axios(`${baseUrl}/students`,options);
-            setStudentList(response.data?.data.students);
-        }
-        catch(err){
-            const {status}=err.response;
-            if(status===401){
-                LogOut();
-            }
-        }
-    };
+    seTotalPagesEvent(totalPages);
+  };
 
-    const getOneStudent = async(id)=>{
-       try{
-            const response = await axios(`${baseUrl}/students/${id}`,options)
-            setStudentDetail(response.data?.data.student)
-        }
-        catch(err){
-            const {status}=err.response;
-            if(status===401){
-                LogOut();
-            }
-        }
-    };
-
-    const getAllEvents=async(limit)=>{
-        try{
-            const response = await axios(`${baseUrl}/events?from=${limit}`,options);
-            setEventsList(response.data?.data.events)
-            const  {totalCount,lengthEventsSent}= response.data.data;
-            calculateTotalPages(totalCount);
-            setTotalEventsGet(lengthEventsSent);
-            setTotalEvents(totalCount);
-        }
-        catch(err){
-            const {status}=err.response;
-            if(status===401){
-                LogOut();
-            }
-        }
-    };
-
-    const getAllEventsByFilter=async(studentName,limit)=>{
-        try{
-            const response = await axios(`${baseUrl}/events?from=${limit}&student=${studentName}`,options);
-            setEventsList(response.data?.data.events)
-            const  {totalCount,lengthEventsSent}= response.data.data;
-            calculateTotalPages(totalCount);
-            setTotalEventsGet(lengthEventsSent);
-            setTotalEvents(totalCount);
-        }
-        catch(err){
-            const {status}=err.response;
-            if(status===401){
-                LogOut();
-            }
-            else if(status===404){
-                setTotalEvents(0);
-                setTotalEventsGet(0);
-            }
-        }
+  const getAllStudents = async () => {
+    try {
+      const response = await axios(`${baseUrl}/students`, options);
+      setStudentList(response.data?.data.students);
+    } catch (err) {
+      const { status } = err.response;
+      if (status === 401) {
+        LogOut();
+      }
     }
+  };
 
-    const getAllAdvisers = async () => {
-        try{
-            const response = await axios(`${baseUrl}/advisers`,options);
-            setAdviserList(response.data?.data.advisers)
-        }
-        catch (err) {
-            const {status} = err.response;
-            if(status===401){
-                LogOut();
-            }        
-        }
-    };
-
-    const getLastStudentAndRedirect= async() => {
-      try{
-        const response = await axios(`${baseUrl}/students`,options);
-        const detailStudent= response.data.data.students;
-        const lastUserId = detailStudent[detailStudent.length-1].id;
-        navigate(`/orientados/${lastUserId}`);
-        window.location.reload();
-        }
-      catch(err){
-        const {status}=err.response;
-        if(status===401){
-                LogOut();
-            }      
-        }
-  
-    };
-
-    return{
-        getAllStudents,
-        getOneStudent,
-        getAllEvents,
-        getAllAdvisers,
-        getLastStudentAndRedirect,
-        getAllEventsByFilter,
-        studentList,
-        studentDetail,
-        eventList,
-        adviserList,
-        totalPagesEvent,
-        totalEventsGet,
-        totalEvents
+  const getOneStudent = async (id) => {
+    try {
+      const response = await axios(`${baseUrl}/students/${id}`, options);
+      setStudentDetail(response.data?.data.student);
+    } catch (err) {
+      const { status } = err.response;
+      if (status === 401) {
+        LogOut();
+      }
     }
+  };
 
+  const getAllEvents = async (limit) => {
+    try {
+      const response = await axios(`${baseUrl}/events?from=${limit}`, options);
+      setEventsList(response.data?.data.events);
+      const { totalCount, lengthEventsSent } = response.data.data;
+      calculateTotalPages(totalCount);
+      setTotalEventsGet(lengthEventsSent);
+      setTotalEvents(totalCount);
+    } catch (err) {
+      const { status } = err.response;
+      if (status === 401) {
+        LogOut();
+      }
+    }
+  };
+
+  const getAllEventsByFilter = async (studentName, limit) => {
+    try {
+      const response = await axios(
+        `${baseUrl}/events?from=${limit}&student=${studentName}`,
+        options
+      );
+      setEventsList(response.data?.data.events);
+      const { totalCount, lengthEventsSent } = response.data.data;
+      calculateTotalPages(totalCount);
+      setTotalEventsGet(lengthEventsSent);
+      setTotalEvents(totalCount);
+    } catch (err) {
+      const { status } = err.response;
+      if (status === 401) {
+        LogOut();
+      } else if (status === 404) {
+        setTotalEvents(0);
+        setTotalEventsGet(0);
+      }
+    }
+  };
+
+  const getAllAdvisers = async () => {
+    try {
+      const response = await axios(`${baseUrl}/advisers`, options);
+      setAdviserList(response.data?.data.advisers);
+    } catch (err) {
+      const { status } = err.response;
+      if (status === 401) {
+        LogOut();
+      }
+    }
+  };
+
+  const getLastStudentAndRedirect = async () => {
+    try {
+      const response = await axios(`${baseUrl}/students`, options);
+      const detailStudent = response.data.data.students;
+      const lastUserId = detailStudent[detailStudent.length - 1].id;
+      navigate(`/orientados/${lastUserId}`);
+      window.location.reload();
+    } catch (err) {
+      const { status } = err.response;
+      if (status === 401) {
+        LogOut();
+      }
+    }
+  };
+
+  return {
+    getAllStudents,
+    getOneStudent,
+    getAllEvents,
+    getAllAdvisers,
+    getLastStudentAndRedirect,
+    getAllEventsByFilter,
+    studentList,
+    studentDetail,
+    eventList,
+    adviserList,
+    totalPagesEvent,
+    totalEventsGet,
+    totalEvents,
+  };
 }
 
-
-export default useGet
+export default useGet;
--- front/src/pages/PublicPages/Welcome/Components/Banner.jsx
+++ front/src/pages/PublicPages/Welcome/Components/Banner.jsx
@@ -7,15 +7,28 @@ import '../../../../index.css';
 import arrowLeft from '../../../../assets/icons/arrow_left.svg';
 import arrowRigth from '../../../../assets/icons/arrow_right.svg';
 
-const featuredImg = [img1, img2, img3];
+// const featuredImg = [img1, img2, img3];
+
+const featuredImg = [
+  {
+    img: img1,
+    text: 'Alentamos tu multipotencialidad. Promovemos tu valor personal',
+  },
+  {
+    img: img2,
+    text: 'Desarrollamos y creamos como estilo de vida'
+  },
+  {
+    img: img3,
+    text: 'Unite a la comunidad de aprendizaje'
+  }
+];
 
 function Banner() {
   const slideRef = useRef();
 
-  const [band, setBand] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  let count;
   const handleOneNextClick = () => {
     const productsLength = featuredImg.length;
     if (currentIndex === productsLength - 1) {
@@ -50,12 +63,13 @@ function Banner() {
         ref={slideRef}
         className='w-full flex justify-center select-none relative'
       >
-        <div className='w-full overflow-hidden object-fit'>
+        <div className='w-full overflow-hidden'>
           <img
-            className='w-[800px] h-[171px] tablet:w-full tablet:h-auto'
-            src={featuredImg[currentIndex]}
+            className='w-[800px] h-[171px] tablet:w-full tablet:h-auto object-cover'
+            src={featuredImg[currentIndex].img}
             alt={currentIndex}
-          ></img>
+          />
+          <p className='text-'>{featuredImg[currentIndex].text}</p>
         </div>
         <div className='absolute w-full tablet:w-4/5 top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center'>
           <button onClick={() => handleOnePrevClick()}>
